103446606be5bc3407ceecfe751e9905d1822f52
Kevin.Chiu <129425482+vimyc95@users.noreply.github.com>
2025-03-07 19:00:47 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,7 @@
+%pred_seg = "/home/rvl224//chiu_2022/affordance_project/result_seg/"
+
+pred_seg = "pred_seg"
+%pred_seg = "/home/rvl224/2TB_disk/affordance_calc_result/IIT_AFF_testset/"
+gt_seg = "gt_seg"
+
+evaluate_Fwb_non_rank(pred_seg, gt_seg)
